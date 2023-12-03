43cf46080a8e20b656804e7092a0aef91203c084
Vladimir <le0679993@crc-lennox.qc.ca>
2023-12-03 01:12:46 -0500

    Added a new template main scene map kind of

--- GreenLevel/Venom/Venom2 2018.gd
+++ GreenLevel/Venom/Venom2 2018.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 
 
-const SPEED = 15.0
+const SPEED = 500.0
 const JUMP_VELOCITY = 4.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -17,6 +17,14 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
+# Handle rotation.
+	var rotation_speed = 150.0  # Adjust this value to your liking.
+	if Input.is_action_pressed("ui_left"):
+		rotation_degrees.y += rotation_speed * delta
+	if Input.is_action_pressed("ui_right"):
+		rotation_degrees.y -= rotation_speed * delta
+
+
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 #	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
@@ -25,6 +33,8 @@ func _physics_process(delta):
 #		velocity.x = direction.x * SPEED
 #		velocity.z = direction.z * SPEED
 	if is_on_floor():
-		velocity.z = gravity * SPEED * delta
-
+		var slope_normal = get_floor_normal() + Vector3.UP *0.01
+		var right_dir = transform.basis.x
+		var forward_dir = right_dir.cross(slope_normal).normalized()
+		velocity = forward_dir * SPEED * delta
 	move_and_slide()
