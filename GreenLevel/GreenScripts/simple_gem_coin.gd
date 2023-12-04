91f5ca089327b517457e1dafb726711609e4777b
RainyCarnival <andy.sherrer@hotmail.ca>
2023-12-04 15:18:18 -0500

    A lot of sound implementation.

--- GreenLevel/GreenScripts/simple_gem_coin.gd
+++ GreenLevel/GreenScripts/simple_gem_coin.gd
@@ -1,5 +1,9 @@
 extends Node3D
 
+signal collected
+
+var is_collected = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -10,4 +14,13 @@ func _process(delta):
 	self.rotate_y(delta * 0.5)
 
 func _on_body_entered(_body):
+	if !is_collected:
+		$"1".visible = false
+		is_collected = true
+		$PickupSound.play()
+		collected.emit()
+		$Timer.start()
+
+
+func _on_timer_timeout():
 	queue_free()
