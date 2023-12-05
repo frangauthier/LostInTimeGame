b69c6bcaebbaea46792b10c8105b9c7667e08ce8
ElijahFrech <efrech05@hotmail.com>
2023-12-05 10:46:07 -0500

    Progress bar draft

--- BlueLevel/target_ui.gd
+++ BlueLevel/target_ui.gd
@@ -1,12 +1,27 @@
 extends CanvasLayer
 
 
-# Called when the node enters the scene tree for the first time.
+var progress_bar : TextureProgressBar
+var time_left : float = 30.0
+
 func _ready():
-	pass # Replace with function body.
+	progress_bar = $MarginContainer2/HBoxContainer2/TextureProgressBar
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$TargetContainer/HBoxContainer/TargetVar.text = str(Globals.targetDestroyed)
+#	$TargetContainer/HBoxContainer/TargetVar.text = str(Globals.targetDestroyed)
+	update_progress_bar()
 	pass
+
+func update_progress_bar():
+	# Calculate the progress based on the remaining time
+	var progress = time_left / 30.0
+	# Update the progress bar value
+	progress_bar.value = progress
+
+func _on_timer_timeout():
+	# Display a message when the timer runs out
+	print("Timer ran out!")
+	# Reload the scene (replace 'blue_level' with your actual scene name)
+	get_tree().change_scene("res://blue_level.tscn")
