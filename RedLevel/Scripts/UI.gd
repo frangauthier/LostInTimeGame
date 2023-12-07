dadde7642bc47d16442429695ca47e8b6b8aa2d8
LE2088826 <le2088826@crc-lennox.qc.ca>
2023-12-07 09:51:30 -0500

    Added text for when the kill count is reached.

--- RedLevel/Scripts/UI.gd
+++ RedLevel/Scripts/UI.gd
@@ -2,3 +2,6 @@ extends CanvasLayer
 
 func _process(delta):
 	$MarginContainer/VBoxContainer/Label.text = "Killed: " + str(GlobalRedVariable.killAmount)
+	
+	if(GlobalRedVariable.killAmount >= 55):
+		$MarginContainer/VBoxContainer/Label.text = "ESCAPE!"
