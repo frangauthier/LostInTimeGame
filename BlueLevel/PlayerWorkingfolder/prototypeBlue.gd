4792dd9d1411544f634cee023f15e6eb0ecd2ec0
lolmile <lolmile32@gmail.com>
2023-12-06 23:02:11 -0500

    Player movement commit

---
+++ BlueLevel/PlayerWorkingfolder/prototypeBlue.gd
@@ -0,0 +1,20 @@
+extends Node3D
+
+var projectile_scene = preload("res://BlueLevel/PlayerWorkingfolder/Scenes/web_projectile.tscn")
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_player_throw(throwTip: Vector3, throw_direction: Vector3):
+	var ball = projectile_scene.instantiate() as RigidBody3D
+	ball.position = throwTip
+	var launch_force:float = 20
+	ball.apply_impulse(throw_direction * launch_force)
+	add_child(ball)
+	pass # Replace with function body.
--- Common/Scripts/Characters/Player.gd
+++ Common/Scripts/Characters/Player.gd
@@ -5,17 +5,25 @@ const LERP_VALUE : float = 0.15
 var snap_vector : Vector3 = Vector3.DOWN
 var speed : float
 
+
 @export_group("Movement variables")
 @export var walk_speed : float = 4.0
 @export var run_speed : float = 10.0
 @export var jump_strength : float = 15.0
 @export var gravity : float = 50.0
+@export var jump_push : float = 300
 
 const ANIMATION_BLEND : float = 7.0
 
 @onready var player_mesh : Node3D = $Sketchfab_model
 @onready var spring_arm_pivot : Node3D = $SpringArmPivot
 @onready var animator : AnimationTree = $AnimationTree
+@onready var animationPlayer : AnimationPlayer = $AnimationPlayer2
+
+var can_move = true
+var can_throw = true
+var is_wall_jump = false
+signal throw
 
 func _physics_process(delta):
 	var move_direction : Vector3 = Vector3.ZERO
@@ -24,33 +32,43 @@ func _physics_process(delta):
 	move_direction = move_direction.rotated(Vector3.UP, spring_arm_pivot.rotation.y)
 	
 	velocity.y -= gravity * delta
-	
-	if Input.is_action_pressed("run"):
-		speed = run_speed
-	else:
-		speed = walk_speed
-	
-	velocity.x = move_direction.x * speed
-	velocity.z = move_direction.z * speed
-	
-	if move_direction:
-		player_mesh.rotation.y = lerp_angle(player_mesh.rotation.y, atan2(velocity.x, velocity.z), LERP_VALUE)
-	
-	var just_landed := is_on_floor() and snap_vector == Vector3.ZERO
-	var is_jumping := is_on_floor() and Input.is_action_just_pressed("jump")
-	if is_jumping:
-		velocity.y = jump_strength
-		snap_vector = Vector3.ZERO
-	elif just_landed:
-		snap_vector = Vector3.DOWN
-	
+	if can_move:
+		if Input.is_action_pressed("run"):
+			speed = run_speed
+		else:
+			speed = walk_speed
+		
+		velocity.x = move_direction.x * speed
+		velocity.z = move_direction.z * speed
+		
+		if move_direction and not is_wall_jump:
+			player_mesh.rotation.y = lerp_angle(player_mesh.rotation.y, atan2(velocity.x, velocity.z), LERP_VALUE)
+		
+		var just_landed := is_on_floor() and snap_vector == Vector3.ZERO
+		var is_jumping := is_on_floor() and Input.is_action_just_pressed("jump")
+		if is_jumping:
+			velocity.y = jump_strength
+			snap_vector = Vector3.ZERO
+		elif just_landed:
+			snap_vector = Vector3.DOWN
+			is_wall_jump = false
+			
 	apply_floor_snap()
 	move_and_slide()
+	wallJump(move_direction.x)
 	animate(delta)
+	
+	for col_idx in get_slide_collision_count():
+		var collision := get_slide_collision(col_idx)
+		if collision.get_collider() is RigidBody3D:
+			collision.get_collider().apply_central_impulse(-collision.get_normal() * 0.3)
+			collision.get_collider().apply_impulse(-collision.get_normal() * 0.01, collision.get_position())
+	
 
 func animate(delta):
 	if is_on_floor():
 		animator.set("parameters/ground_air_transition/transition_request", "grounded")
+		shoot()
 		
 		if velocity.length() > 0:
 			if speed == run_speed:
@@ -61,3 +79,36 @@ func animate(delta):
 			animator.set("parameters/iwr_blend/blend_amount", lerp(animator.get("parameters/iwr_blend/blend_amount"), -1.0, delta * ANIMATION_BLEND))
 	else:
 		animator.set("parameters/ground_air_transition/transition_request", "air")
+
+func shoot():
+	if Input.is_action_pressed("LeftClick") and can_throw:
+		velocity = Vector3.ZERO
+		can_throw = false
+		can_move = false
+		animationPlayer.play("Throw")
+		
+func setMoving():
+	can_move = true
+
+func shootBall():
+	var throwTip = $Sketchfab_model/throwTip.global_position
+	var throw_direction : Vector3 = Vector3.ZERO
+	throw_direction = -player_mesh.basis.z
+	throw_direction.y = $SpringArmPivot.rotation_degrees.x
+	#TTOOO DOOOOO
+	
+	throw.emit(throwTip, throw_direction.normalized())
+	$Timer.start()
+	
+func wallJump(direction):
+	if is_on_wall_only() and Input.is_action_just_pressed("jump") and not is_wall_jump:
+		is_wall_jump = true
+		velocity.y = jump_strength
+		animationPlayer.play("swingEnd")
+		animationPlayer.advance(0.6)
+		print(direction)
+		velocity.x = -direction * jump_push
+		
+func _on_timer_timeout():
+	can_throw = true
+	pass # Replace with function body.
