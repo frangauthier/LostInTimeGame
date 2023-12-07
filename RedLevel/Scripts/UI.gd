949a588e2e1ff073886a8bf68b359bddb52fc88a
LE2088826 <le2088826@crc-lennox.qc.ca>
2023-12-07 09:55:08 -0500

    Changed Escape text code to work with updated UI setup.

--- RedLevel/Scripts/UI.gd
+++ RedLevel/Scripts/UI.gd
@@ -4,4 +4,4 @@ func _process(delta):
 	$PanelContainer/Label.text = "Dummies Smashed: " + str(GlobalRedVariable.killAmount)
 	
 	if(GlobalRedVariable.killAmount >= 55):
-		$MarginContainer/VBoxContainer/Label.text = "ESCAPE!"
+		$PanelContainer/Label2.text = "ESCAPE!"
