2f93837067c4958e0d61fe84ded0092b7449460b
MattyPye5 <matt.pye@hotmail.com>
2023-12-06 19:01:26 -0500

    Modified Text added stage music

--- RedLevel/Scripts/UI.gd
+++ RedLevel/Scripts/UI.gd
@@ -1,4 +1,4 @@
 extends CanvasLayer
 
 func _process(delta):
-	$MarginContainer/VBoxContainer/Label.text = "Killed: " + str(GlobalRedVariable.killAmount)
+	$MarginContainer2/VBoxContainer/Label2.text = "Dummies Smashed: " + str(GlobalRedVariable.killAmount)
