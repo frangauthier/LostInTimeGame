59d60591fca08f978790838a4f9b4a619a82458d
LE2088826 <le2088826@crc-lennox.qc.ca>
2023-12-07 10:33:17 -0500

    Updated goal text to match killGoal.

--- RedLevel/Scripts/UI.gd
+++ RedLevel/Scripts/UI.gd
@@ -3,5 +3,7 @@ extends CanvasLayer
 func _process(delta):
 	$PanelContainer/Label.text = "Dummies Smashed: " + str(GlobalRedVariable.killAmount)
 	
-	if(GlobalRedVariable.killAmount >= GlobalRedVariable.killGoal):
+	if(GlobalRedVariable.killAmount <= GlobalRedVariable.killGoal):
+		$PanelContainer/Label2.text = "DESTORY " + str(GlobalRedVariable.killGoal) + " DUMMIES"
+	else:
 		$PanelContainer/Label2.text = "ESCAPE!"
